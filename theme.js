25d248cb2a3a9e512675ff7c9c5331ba9863cf2c
Andres Osorio <andres.osorio@taxfix.de>
2020-05-26 13:28:56 +0200

    modify theme

--- theme.js
+++ theme.js
@@ -4,11 +4,21 @@ import produce from "immer";
 export default produce(themes.swiss, draft => {
   draft.colors = {
     background: "#1c2033",
-    text: "#32C850"
+    primary: "#32C850",
+    text: "#fff"
   }
 
   draft.fonts = {
     body: "CircularStd-Book, arial, sans-serif"
+  }
+
+  draft.styles = {
+    title: {
+      color: "primary"
+    },
+    subtitle: {
+      color: "primary"
+    },
   }
 
   draft.components = {
